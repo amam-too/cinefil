1b77c5ad64fd95701ebc70cd3c1928b5254eb38c
Matis Byar <matis.byar.06@icloud.com>
2024-10-23 17:44:43 +0200

    improved: navbar style, adapted it for NextUI

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,7 +3,7 @@ import Search from "@/components/search";
 
 export default function Navbar() {
     return (
-        <div className="flex items-center justify-between w-full h-16 px-12 border-b border-stone-800 text-white fixed backdrop-blur-2xl">
+        <div className="flex items-center justify-between w-full h-16 px-12 text-white backdrop-blur-2xl">
             <div className="flex items-center">
                 <h1 className="text-xl font-bold">cinéfil</h1>
             </div>
