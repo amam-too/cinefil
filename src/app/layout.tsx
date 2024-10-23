b85e025197603bf7d4f32a5af679ef7239cd2f0f
Matis Byar <matis.byar.06@icloud.com>
2024-10-23 17:44:01 +0200

    new: added NextUI Provider.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,32 +2,37 @@ import "@/styles/globals.css";
 import Navbar from "@/components/navbar";
 import { ThemeProvider } from "@/components/themeProvider";
 import { Toaster } from "@/components/ui/sonner";
+import { NextUIProvider } from "@nextui-org/system";
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
 import React, { type ReactNode, Suspense } from "react";
 
 export const metadata: Metadata = {
-    title: "cinéfil",
-    description: "application officielle"
+  title: "cinéfil",
+  description: "application officielle",
 };
 
-export default async function RootLayout({children}: Readonly<{ children: ReactNode }>) {
-    return (
-        <html lang="fr" className={ `${ GeistSans.variable } bg-stone-950` }>
-        <body>
-            <ThemeProvider
-                attribute="class"
-                defaultTheme="dark"
-                enableSystem
-                disableTransitionOnChange
-            >
-                <Suspense>
-                    <Navbar/>
-                </Suspense>
-                { children }
-            </ThemeProvider>
-            <Toaster/>
-        </body>
-        </html>
-    );
+export default async function RootLayout({
+  children,
+}: Readonly<{ children: ReactNode }>) {
+  return (
+    <html lang="fr" className={`${GeistSans.variable} bg-stone-950`}>
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <NextUIProvider>
+            <Suspense>
+              <Navbar />
+            </Suspense>
+            {children}
+          </NextUIProvider>
+        </ThemeProvider>
+        <Toaster />
+      </body>
+    </html>
+  );
 }
