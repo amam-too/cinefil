4d6b92622c1de661049afa673f48131f8e28bab4
Matis Byar <matis.byar.06@icloud.com>
2024-10-27 16:30:36 +0100

    fixed: reinstated middleware function

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -1,44 +1,47 @@
-"use server"
-
-import { createClient } from "@/utils/supabase/server";
-import { type NextRequest, NextResponse } from 'next/server'
+import { createServerClient } from '@supabase/ssr'
+import { NextResponse, type NextRequest } from 'next/server'
 
 export async function updateSession(request: NextRequest) {
-    const supabaseResponse = NextResponse.next({
-        request
+    let supabaseResponse = NextResponse.next({
+        request,
     })
     
-    // const supabase = createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
-    //     cookies: {
-    //         getAll() {
-    //             return request.cookies.getAll();
-    //         },
-    //         setAll(cookiesToSet) {
-    //             cookiesToSet.forEach(({name, value, options}) => request.cookies.set(name, value));
-    //             supabaseResponse = NextResponse.next({
-    //                 request
-    //             });
-    //             cookiesToSet.forEach(({name, value, options}) => supabaseResponse.cookies.set(name, value, options));
-    //         }
-    //     }
-    // });
+    const supabase = createServerClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        {
+            cookies: {
+                getAll() {
+                    return request.cookies.getAll()
+                },
+                setAll(cookiesToSet) {
+                    cookiesToSet.forEach(({ name, value}) => request.cookies.set(name, value))
+                    supabaseResponse = NextResponse.next({
+                        request,
+                    })
+                    cookiesToSet.forEach(({ name, value, options }) =>
+                        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+                        supabaseResponse.cookies.set(name, value, options)
+                    )
+                },
+            },
+        }
+    )
     
     // IMPORTANT: Avoid writing any logic between createServerClient and
     // supabase.auth.getUser(). A simple mistake could make it very hard to debug
     // issues with users being randomly logged out.
     
-    const supabase = createClient()
-    
     const {
-        data: {user}
+        data: { user },
     } = await supabase.auth.getUser()
     
     if (
         !user &&
         !request.nextUrl.pathname.startsWith('/login') &&
-        !request.nextUrl.pathname.startsWith('/auth')
+        !request.nextUrl.pathname.startsWith('/api/auth')
     ) {
-        // no user, potentially respond by redirecting the user to the login page
+        // No user, potentially respond by redirecting the user to the login page.
         const url = request.nextUrl.clone()
         url.pathname = '/login'
         return NextResponse.redirect(url)
