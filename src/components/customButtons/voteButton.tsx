30d81b2773c41d969ff8885642bf0ccff1b19558
Matis Byar <matis.byar.06@icloud.com>
2024-12-14 13:40:15 +0100

    Improved Voting system with a Loading wheel

--- src/components/customButtons/voteButton.tsx
+++ src/components/customButtons/voteButton.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import LoadingWheel from "@/components/loadingWheel";
 import { Button } from "@/components/ui/button";
 import { voteForMovie } from "@/server/services/votes";
 import { useState } from "react";
@@ -7,19 +8,25 @@ import { toast } from "sonner";
 
 export default function VoteButton({movieId}: { movieId: number }) {
     const [hasVoted, setHasVoted] = useState(false);
+    const [loading, setLoading] = useState(false);
     
     return !hasVoted ? (
         <Button onClick={ async () => {
+            setLoading(true);
             toast.promise(voteForMovie(movieId.toString()), {
                 loading: "On enregistre ton vote...",
                 success: (response) => {
                     setHasVoted(true);
+                    setLoading(false);
                     return response.message;
                 },
-                error: (error) => `${error.message}`,
+                error: (error) => {
+                    setLoading(false);
+                    return error.message
+                }
             });
         } }>
-            Voter
+            { loading ? <LoadingWheel/> : "Voter" }
         </Button>
     ) : null
 }
