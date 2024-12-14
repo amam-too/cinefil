178972c50902952d27251d6182c0b90224a8b964
Matis Byar <matis.byar.06@icloud.com>
2024-12-14 13:22:05 +0100

    Added a Votes service that has voteForMovie, hasVotedForMovie and getNumberOfVotesForCurrentUser functions.

--- src/server/services/votes.ts
+++ src/server/services/votes.ts
@@ -1,7 +1,5 @@
-"use server";
-
 import { type VoteForMovieResponse } from "@/types/responses";
-import { createClient } from "@/utils/supabase/server";
+import { createClient } from "@/utils/supabase/client";
 
 /**
  * Votes for one movie.
@@ -17,7 +15,16 @@ export async function voteForMovie(tmdb_id: string): Promise<VoteForMovieRespons
         console.error(sessionError)
         return {
             success: false,
-            message: "Une erreur est survenue, merci de réessayer ultérieurement."
+            message: "Impossible de lire la session de l'utilisateur. Essayez de vous reconnecter."
+        }
+    }
+    
+    const numberOfVotes = await getNumberOfVotesForCurrentUser(session.user.id);
+    
+    if (numberOfVotes >= 3) {
+        return {
+            success: false,
+            message: "Tu as déjà voté pour 3 films. Tu ne peux plus voter."
         }
     }
     
@@ -32,7 +39,7 @@ export async function voteForMovie(tmdb_id: string): Promise<VoteForMovieRespons
         console.error(error)
         return {
             success: false,
-            message: "Une erreur est survenue, merci de réessayer ultérieurement."
+            message: "Une erreur est survenue, merci de réessayer ultérieurement. Le vote n'a pas été pris en compte."
         }
     }
     
@@ -42,35 +49,39 @@ export async function voteForMovie(tmdb_id: string): Promise<VoteForMovieRespons
     }
 }
 
-export async function unvoteForMovie(tmdb_id: string): Promise<VoteForMovieResponse> {
+export async function hasVotedForMovie(tmdb_id: string, user_id: string): Promise<boolean> {
     const supabase = createClient()
     
-    const {data: session, error: sessionError} = await supabase.auth.getUser();
-    
-    if (sessionError || !session) {
-        console.error(sessionError)
-        return {
-            success: false,
-            message: "Une erreur est survenue, merci de réessayer ultérieurement."
-        }
-    }
-    
-    const { error } = await supabase
+    const {data: votes, error: votesError} = await supabase
       .from("votes")
-      .delete()
+      .select("*")
       .eq("tmdb_id", tmdb_id)
-      .eq("user_id", session.user.id);
+      .eq("user_id", user_id);
     
-    if (error) {
-        console.error(error)
-        return {
-            success: false,
-            message: "Une erreur est survenue, merci de réessayer ultérieurement."
-        }
+    if (votesError) {
+        console.error(votesError)
+        return false
     }
     
-    return {
-        success: true,
-        message: "Ton vote a été retiré."
+    return votes && votes.length > 0
+}
+
+/**
+ * Returns the number of votes for the current user.
+ * @returns {Promise<number>}
+ */
+async function getNumberOfVotesForCurrentUser(user_id: string): Promise<number> {
+    const supabase = createClient()
+    
+    const {data: votes, error: votesError} = await supabase
+      .from("votes")
+      .select("*")
+      .eq("user_id", user_id);
+    
+    if (votesError) {
+        console.error(votesError)
+        return 0
     }
+    
+    return votes ? votes.length : 0
 }
