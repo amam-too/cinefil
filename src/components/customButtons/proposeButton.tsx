de57ca08e8c33caa20314b12a0a7a907810da794
Matis Byar <matis.byar.06@icloud.com>
2024-12-21 16:25:06 +0100

    Renamed Suggestion to Proposition

--- src/components/customButtons/proposeButton.tsx
+++ src/components/customButtons/proposeButton.tsx
@@ -6,32 +6,30 @@ import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { toast } from "sonner";
 import { type MovieDetails } from "tmdb-ts";
 
-export default function SuggestButton({movieDetails}: { movieDetails: MovieDetails }) {
+export default function ProposeButton({movieDetails, onExceedLimit}: { movieDetails: MovieDetails, onExceedLimit: (currentPropositions: any) => void }) {
     const searchParams = useSearchParams();
     const pathname = usePathname();
     const router = useRouter();
     
-    /**
-     *
-     */
-    const displaySuggestions = (): void => {
+    const displayPropositions = (): void => {
         const params = new URLSearchParams(searchParams);
         params.delete(SearchParams.QUERY);
         params.delete(SearchParams.FILM_ID);
         router.replace(`${ pathname }?${ params.toString() }`);
-        
-        // TODO : Remove query inside searchbar too.
     };
     
-    /**
-     *
-     * @param movie
-     */
     const createdSuggestion = async (movie: MovieDetails): Promise<void> => {
+        const currentPropositions = await getCurrentPropositions()
+        if (currentPropositions?.length >= 3) {
+            onExceedLimit(currentPropositions);
+            return;
+        }
+        
         const {error} = await createClient().from("suggestions").insert({
             tmdb_id: movie.id,
             shown_at: null,
-            createdAt: new Date().toISOString()
+            created_at: new Date().toISOString(),
+            user_id: (await createClient().auth.getUser()).data.user?.id
         });
         
         if (error) {
@@ -39,21 +37,22 @@ export default function SuggestButton({movieDetails}: { movieDetails: MovieDetai
                 toast.warning(`${ movie.title } est déjà ajouté aux suggestions.`, {
                     action: {
                         label: "Voir les suggestions",
-                        onClick: displaySuggestions
+                        onClick: displayPropositions
                     }
                 });
                 return;
             }
             
-            toast.error("Impossible d'ajouter la suggestion !", {
-                description: `${ movie.title } n'a pas pu être ajouté aux suggestions.`
+            toast.error("Une erreur est survenue.", {
+                description: `${ movie.title } n'a pas pu être ajouté aux propositions.`
             });
+            throw new Error(error.message);
         } else {
-            toast.success("Suggestion ajoutée !", {
-                description: `${ movie.title } a été ajouté aux suggestions.`,
+            toast.success("Attendons les votes !", {
+                description: `${ movie.title } a été ajouté aux propositions.`,
                 action: {
                     label: "Voir les suggestions",
-                    onClick: displaySuggestions
+                    onClick: displayPropositions
                 }
             });
         }
@@ -68,4 +67,22 @@ export default function SuggestButton({movieDetails}: { movieDetails: MovieDetai
             Proposer le film
         </Button>
     );
+}
+
+async function getCurrentPropositions() {
+    const {data: userData, error: userError} = await createClient().auth.getUser();
+    
+    if (!userData.user?.id || userError) {
+        throw new Error(userError?.message ?? "User not found.");
+    }
+    
+    const {data, error} = await createClient().from("suggestions").select()
+        .eq("user_id", userData.user.id)
+        .is("shown_at", null);
+    
+    if (error) {
+        throw new Error(error.message);
+    }
+    
+    return data;
 }
