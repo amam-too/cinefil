1cbe1276961070831f037864939d905954623484
Matis Byar <matis.byar.06@icloud.com>
2024-12-21 14:34:42 +0100

    Lint

--- src/components/propositions.tsx
+++ src/components/propositions.tsx
@@ -5,46 +5,26 @@ import { createClient } from "@/utils/supabase/server";
 import React, { Suspense } from "react";
 import { type Movie } from "tmdb-ts";
 
-export default async function Propositions({displayShown}: {
-    displayShown?: string | undefined,
-}) {
-    
-    /**
-     * Fetch movie details for each suggestion based on tmdb_id.
-     * @param suggestions
-     */
-    const fetchMoviesDetails = async (suggestions: Suggestion[]) => {
-        const moviePromises = suggestions.map(async (suggestion) => {
-            return await getMovieDetails(suggestion.tmdb_id);
-        });
-        
-        return await Promise.all(moviePromises);
+export default async function Propositions({ displayShown }: { displayShown?: string }) {
+    const fetchMoviesDetails = (suggestions: Suggestion[]) => {
+        const moviePromises = suggestions.map(suggestion => getMovieDetails(suggestion.tmdb_id));
+        return Promise.all(moviePromises);
     };
-    
-    /**
-     *
-     */
+
     const fetchSuggestions = async () => {
-        const {data, error} = await createClient().from("suggestions").select()
-            .order('shown_at', {ascending: false});  // Sort by shown_at in descending order.
-        
+        const { data, error } = await createClient().from("suggestions").select().order('shown_at', { ascending: false });
         if (error) {
             console.error("Error fetching suggestions:", error);
-            // TODO : Add error handlingg.
-            return <>{ error }</>;
+            return [];
         }
-        
-        if (data && data.length > 0) {
-            return fetchMoviesDetails(data as Suggestion[]);
-        }
-    }
-    
+        return data && data.length > 0 ? fetchMoviesDetails(data as Suggestion[]) : [];
+    };
+
     return (
         <div className="flex flex-col">
             <h1 className="text-2xl font-bold mt-4 ml-8">Films proposés</h1>
-            <Suspense>
-                <MoviesGrid movies={ await fetchSuggestions() as unknown[] as Movie[] } forSuggestions={ true }
-                            displayShown={ displayShown }/>
+            <Suspense fallback={<p>Loading...</p>}>
+                <MoviesGrid movies={await fetchSuggestions() as Movie[]} forSuggestions={true} displayShown={displayShown} />
             </Suspense>
         </div>
     );
