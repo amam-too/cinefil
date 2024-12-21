5b6c407ecbc4383b191ce52199e31875b0a489f0
Matis Byar <matis.byar.06@icloud.com>
2024-12-21 11:25:35 +0100

    Improved visual aspect of the movies card.

--- src/components/movie/movieCard.tsx
+++ src/components/movie/movieCard.tsx
@@ -1,60 +1,70 @@
 "use client";
 
+import { SearchParams } from "@/app/searchParams";
 import SuggestButton from "@/components/customButtons/suggestButton";
-import {Card, CardContent, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
-import {usePathname, useRouter, useSearchParams} from "next/navigation";
-import {type Movie, type MovieDetails} from "tmdb-ts";
-import {useDebouncedCallback} from "use-debounce";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { getSuggestions } from "@/server/services/tmdb";
+import { getHumanReadableDate, getYearOnly } from "@/utils/date";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import React from "react";
-import {getHumanReadableDate, getYearOnly} from "@/utils/date";
-import {SearchParams} from "@/app/searchParams";
+import { type Movie, type MovieDetails } from "tmdb-ts";
+import { useDebouncedCallback } from "use-debounce";
 
-export default function MoviesCard(
-    {movie, hasBeenSuggested, shown_at}:
-        { movie: Movie; hasBeenSuggested: boolean; shown_at?: string | undefined }) {
+interface MovieCardProps {
+    movie: Movie;
+    hasBeenSuggested: boolean;
+    shown_at?: string;
+}
+
+export default async function MoviesCard({movie, hasBeenSuggested, shown_at}: MovieCardProps) {
     const searchParams = useSearchParams();
     const pathname = usePathname();
     const router = useRouter();
-
+    
     const selectMovie = useDebouncedCallback((filmId: number): void => {
         const params = new URLSearchParams(searchParams);
-
+        
         if (filmId) {
             params.set(SearchParams.FILM_ID, filmId.toString());
         } else {
             params.delete(SearchParams.FILM_ID);
         }
-
-        router.replace(`${pathname}?${params.toString()}`);
+        
+        router.replace(`${ pathname }?${ params.toString() }`);
     }, 300);
-
+    
     return (
         <Card
-            key={movie.id}
+            className="cursor-pointer flex flex-col justify-between "
+            key={ movie.id }
         >
-            <div onClick={(): void => selectMovie(movie.id)}>
-                <CardContent>
-                    {/* eslint-disable-next-line @next/next/no-img-element */}
+            <div onClick={ (): void => selectMovie(movie.id) }>
+                <CardContent className="flex flex-col p-0">
+                    {/* eslint-disable-next-line @next/next/no-img-element */ }
                     <img
-                        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                        alt={movie.title}
-                        className={`rounded-lg p-4 ${shown_at ? 'grayscale opacity-50' : ''}`}
+                        src={ `https://image.tmdb.org/t/p/w500${ movie.poster_path }` }
+                        alt={ movie.title }
+                        className={ `rounded-lg rounded-b-none max-h-40 object-cover h-full w-full ${ shown_at ? 'grayscale opacity-50' : '' }` }
                     />
+                    
+                    <div className="p-2 px-3">
+                        <h1 className="font-semibold text-lg">{ movie.title }</h1>
+                        <p className="text-start text-sm text-gray-500">{ getYearOnly(movie.release_date) }</p>
+                        { shown_at ? <p className="text-start text-sm text-red-500 font-bold pt-8">
+                            Diffusé { getHumanReadableDate(shown_at) }
+                        </p> : null }
+                    </div>
                 </CardContent>
-                <CardHeader>
-                    <CardTitle className={"text-xl"}>{movie.title}</CardTitle>
-                    <p className="text-start text-sm text-gray-500">{getYearOnly(movie.release_date)}</p>
-                    {shown_at ? <p className="text-start text-sm text-red-500 font-bold pt-8">
-                        Diffusé lors de la séance du : {getHumanReadableDate(shown_at)}
-                    </p> : null}
-                </CardHeader>
             </div>
-            <CardFooter>
-                {hasBeenSuggested || shown_at ? (
-                    <>{/* TODO : Add Vote Button. */}</>
+            <CardFooter className="p-4 pb-4">
+                { hasBeenSuggested || shown_at ? (
+                    <>
+                        {/* TODO : Add Vote Button. */ }
+                        Vote
+                    </>
                 ) : (
-                    <SuggestButton movieDetails={movie as unknown as MovieDetails}/>
-                )}
+                    <SuggestButton movieDetails={ movie as unknown as MovieDetails }/>
+                ) }
             </CardFooter>
         </Card>
     );
