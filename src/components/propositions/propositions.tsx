4573e056a8d4931848df3d4bc17c260f4f444c29
Matis Byar <matis.byar.06@icloud.com>
2024-12-21 16:25:34 +0100

    Renamed Suggestion to Proposition

--- src/components/propositions/propositions.tsx
+++ src/components/propositions/propositions.tsx
@@ -6,26 +6,26 @@ import React, { Suspense } from "react";
 import { type Movie } from "tmdb-ts";
 import { getSuggestions } from "@/server/services/suggestions";
 
-export default async function Propositions({ displayShown }: { displayShown?: string }) {
+export default async function Propositions({displayShown}: { displayShown?: string }) {
     const fetchMoviesDetails = (suggestions: Suggestion[]) => {
         const moviePromises = suggestions.map(suggestion => getMovieDetails(suggestion.tmdb_id));
         return Promise.all(moviePromises);
     };
-
+    
     const fetchSuggestions = async () => {
-        const { data, error } = await createClient().from("suggestions").select().order('shown_at', { ascending: false });
+        const {data, error} = await createClient().from("suggestions").select().order('shown_at', {ascending: false});
         if (error) {
             console.error("Error fetching suggestions:", error);
             return [];
         }
         return data && data.length > 0 ? fetchMoviesDetails(data as Suggestion[]) : [];
     };
-
+    
     return (
         <div className="flex flex-col">
             <h1 className="text-2xl font-bold mt-4 ml-8">Films proposés</h1>
-            <Suspense fallback={<p>Loading...</p>}>
-                <MoviesGrid movies={await fetchSuggestions() as Movie[]} forSuggestions={true} displayShown={displayShown} />
+            <Suspense fallback={ <p>Loading...</p> }>
+                <MoviesGrid movies={ await fetchSuggestions() as unknown as Movie[] } forSuggestions={ true } displayShown={ displayShown }/>
             </Suspense>
         </div>
     );
