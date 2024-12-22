bbcef43b2919ab71beda8d98d1996e18c0f26186
Matis Byar <matis.byar.06@icloud.com>
2024-12-22 16:47:31 +0100

    Added the functionality to propose a movie.
    The logic is as follows:
    The user proposes a movie
    The server receives the proposition and checks whether the user has less than 3 movies already currently proposed.
    If he has 3 or more movies, the client displays a modal to ask him this: either they cancel their proposition OR they choose to remove one of their current propositions
    When this is done, the server receives the instruction and acts consequently. If one movie was chosen, it is removed from the "suggestions" table and the new one is added. If the proposition is canceled, nothing more happens.

--- src/types/responses.ts
+++ src/types/responses.ts
@@ -1,4 +1,15 @@
+import { Suggestion } from "@/types/suggestion";
+
 export type VoteForMovieResponse = {
     success: boolean;
     message: string;
+}
+
+export type ProposeAMovieResponse = {
+    success: boolean;
+    message: string;
+} | {
+    success: false;
+    message: string;
+    propositions: Suggestion[]
 }
