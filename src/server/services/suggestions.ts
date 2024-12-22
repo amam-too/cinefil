cf7e558252d451070d02e1456b547d22844a8bdd
Maxime Bodin <maxime.bodin@mailo.com>
2024-12-23 00:35:08 +0100

    Linting on suggestions.ts.

--- src/server/services/suggestions.ts
+++ src/server/services/suggestions.ts
@@ -9,18 +9,18 @@ import { type SuggestMovieResponse } from "@/types/responses";
  * @returns {Promise<Suggestion[]>} A list of all suggestions.
  */
 export async function getSuggestions(): Promise<Suggestion[]> {
-  const supabase = createClient()
+  const supabase = createClient();
 
   const { data: suggestions, error: suggestionsError } = await supabase
     .from("suggestions")
-    .select("*")
+    .select("*");
 
   if (suggestionsError) {
-    console.error("Error fetching suggestions:", suggestionsError)
-    return []
+    console.error("Error fetching suggestions:", suggestionsError);
+    return [];
   }
 
-  return suggestions as Suggestion[]
+  return suggestions as Suggestion[];
 }
 
 /**
@@ -34,33 +34,33 @@ export async function getSuggestions(): Promise<Suggestion[]> {
 export async function getUserSuggestions(
   suggestions?: Suggestion[],
 ): Promise<Suggestion[]> {
-  const supabase = createClient()
+  const supabase = createClient();
 
-  const { data: session, error: sessionError } = await supabase.auth.getUser()
+  const { data: session, error: sessionError } = await supabase.auth.getUser();
 
   if (sessionError || !session?.user) {
     throw new Error(
       "Unable to retrieve the user session. Please try reconnecting.",
-    )
+    );
   }
 
   if (suggestions) {
     return suggestions.filter(
       (suggestion) => suggestion.user_id === session.user.id,
-    )
+    );
   }
 
   const { data: dbSuggestions, error: dbError } = await supabase
     .from("suggestions")
     .select("*")
-    .eq("user_id", session.user.id)
+    .eq("user_id", session.user.id);
 
   if (dbError) {
-    console.error("Error fetching user suggestions:", dbError)
-    return []
+    console.error("Error fetching user suggestions:", dbError);
+    return [];
   }
 
-  return dbSuggestions as Suggestion[]
+  return dbSuggestions as Suggestion[];
 }
 
 /**
@@ -71,14 +71,14 @@ export async function getUserSuggestions(
 export async function suggestMovie(
   tmdb_id: string,
 ): Promise<SuggestMovieResponse> {
-  const supabase = createClient()
+  const supabase = createClient();
 
-  const { data: session, error: sessionError } = await supabase.auth.getUser()
+  const { data: session, error: sessionError } = await supabase.auth.getUser();
 
   if (sessionError || !session?.user) {
     throw new Error(
       "Unable to retrieve the user session. Please try reconnecting.",
-    )
+    );
   }
 
   const { error } = await supabase.from("suggestions").insert({
@@ -86,25 +86,25 @@ export async function suggestMovie(
     shown_at: null,
     created_at: new Date().toISOString(),
     user_id: session.user.id,
-  })
+  });
 
   if (error) {
-    console.error("Error suggesting movie:", error)
+    console.error("Error suggesting movie:", error);
 
     if (error.code === "23505") {
       // Duplicate entry
-      throw new Error("This movie has already been suggested.")
+      throw new Error("This movie has already been suggested.");
     }
 
     throw new Error(
       "An error occurred. Please try again later. The movie suggestion was not recorded.",
-    )
+    );
   }
 
   return {
     success: true,
     message: "Movie successfully suggested!",
-  }
+  };
 }
 
 /**
@@ -115,32 +115,32 @@ export async function suggestMovie(
 export async function removeSuggestion(
   tmdb_id: string,
 ): Promise<SuggestMovieResponse> {
-  const supabase = createClient()
+  const supabase = createClient();
 
-  const { data: session, error: sessionError } = await supabase.auth.getUser()
+  const { data: session, error: sessionError } = await supabase.auth.getUser();
 
   if (sessionError || !session?.user) {
     throw new Error(
       "Unable to retrieve the user session. Please try reconnecting.",
-    )
+    );
   }
 
   const { error } = await supabase
     .from("suggestions")
     .delete()
     .eq("tmdb_id", tmdb_id)
-    .eq("user_id", session.user.id) // Ensure the deletion is scoped to the current user.
+    .eq("user_id", session.user.id); // Ensure the deletion is scoped to the current user.
 
   if (error) {
-    console.error("Error removing suggestion:", error)
+    console.error("Error removing suggestion:", error);
 
     throw new Error(
       "An error occurred. Please try again later. The movie suggestion was not removed.",
-    )
+    );
   }
 
   return {
     success: true,
     message: "Movie suggestion successfully removed!",
-  }
+  };
 }
