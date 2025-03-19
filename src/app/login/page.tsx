dda2b51f37c201a0f2f368c1ffe1119bf33d1d91
Matis Byar <matis.byar.06@icloud.com>
2025-03-19 13:38:19 +0100

    fix: refactor login button to use async onClick handler for proper async handling

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,39 +1,30 @@
-"use server";
-
 import { login } from "@/app/login/actions";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { DiscordLogoIcon } from "@radix-ui/react-icons";
 
 export default async function LoginPage() {
-  return (
-    <div className="flex h-screen items-center justify-center pt-20">
-      <Card className="flex w-[500px] flex-col items-center">
-        <CardHeader className="items-center">
-          <CardTitle className="text-xl font-bold">cinéfil</CardTitle>
-          <CardDescription>Hi! Sign in to continue</CardDescription>
-        </CardHeader>
-        <CardContent className="w-full">
-          <form className="flex flex-col gap-2">
-            <Button
-              variant="outline"
-              className="flex items-center gap-2 p-2"
-              formAction={() => {
-                "use server";
-                return login("discord");
-              }}
-            >
-              <DiscordLogoIcon /> Sign in with Discord
-            </Button>
-          </form>
-        </CardContent>
-      </Card>
-    </div>
-  );
+    const onClick = async () => {
+        await login("discord");
+    }
+    
+    return (
+        <div className="flex h-screen items-center justify-center pt-20">
+            <Card className="flex w-[500px] flex-col items-center">
+                <CardHeader className="items-center">
+                    <CardTitle className="text-xl font-bold">cinéfil</CardTitle>
+                    <CardDescription>Hi! Sign in to continue</CardDescription>
+                </CardHeader>
+                <CardContent className="w-full">
+                    <Button
+                        variant="outline"
+                        className="flex items-center gap-2 p-2"
+                        onClick={ onClick }
+                    >
+                        <DiscordLogoIcon/> Sign in with Discord
+                    </Button>
+                </CardContent>
+            </Card>
+        </div>
+    );
 }
