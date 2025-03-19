eaeec7a2441b9538a45df3ac7a151c70d95bcecd
Matis Byar <matis.byar.06@icloud.com>
2025-03-19 09:43:15 +0100

    fix: removed theme provider which caused issues with hydration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,38 +1,30 @@
 import "@/styles/globals.css";
 import Navbar from "@/components/navbar";
-import { ThemeProvider } from "@/components/themeProvider";
 import { Toaster } from "@/components/ui/sonner";
 import { NextUIProvider } from "@nextui-org/system";
-import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
+import { Geist } from "next/font/google";
 import React, { type ReactNode, Suspense } from "react";
 
 export const metadata: Metadata = {
-  title: "cinéfil",
-  description: "application officielle",
+    title: "cinéfil",
+    description: "application officielle"
 };
 
-export default async function RootLayout({
-  children,
-}: Readonly<{ children: ReactNode }>) {
-  return (
-    <html lang="fr" className={`${GeistSans.variable} bg-stone-950`}>
-      <body>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <NextUIProvider>
-            <Suspense>
-              <Navbar />
-            </Suspense>
-            {children}
-          </NextUIProvider>
-        </ThemeProvider>
-        <Toaster />
-      </body>
-    </html>
-  );
+const geist = Geist({subsets: ["latin"]})
+
+export default async function RootLayout({children}: Readonly<{ children: ReactNode }>) {
+    return (
+        <html lang="fr" className={ `${ geist.className } bg-stone-950 dark` }>
+        <body>
+            <NextUIProvider>
+                <Suspense>
+                    <Navbar/>
+                </Suspense>
+                { children }
+            </NextUIProvider>
+            <Toaster/>
+        </body>
+        </html>
+    );
 }
