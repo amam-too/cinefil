480f34b16665239c806e1d2a6105bc03b504beec
Matis Byar <matis.byar.06@icloud.com>
2025-03-23 19:24:13 +0100

    feat: configure remote image patterns for TMDB in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    trailingSlash: false
+    trailingSlash: false,
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "image.tmdb.org",
+            }
+        ]
+    }
 };
 
 export default nextConfig;
