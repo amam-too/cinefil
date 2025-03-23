da3f77e27cb8a5191e2a1d6c6a448a8c246b894a
Matis Byar <matis.byar.06@icloud.com>
2025-03-23 15:01:50 +0100

    refactor: update Navbar component structure and improve accessibility

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,15 +1,26 @@
-import AccountDropdown from "@/components/accountDropdown";
-import Search from "@/components/search";
+import AccountDropdown from "@/components/auth/account-dropdown";
+import logo from "@/public/logo.svg";
+import { SearchIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { Suspense } from "react";
 
 export default function Navbar() {
     return (
-        <div className="flex items-center justify-between w-full h-16 px-12 text-white backdrop-blur-2xl">
+        <div className="flex items-center justify-between w-full h-16 px-12 backdrop-blur-2xl">
             <div className="flex items-center">
-                <h1 className="text-xl font-bold">cinéfil</h1>
+                <Link href="/" className="px-4">
+                    <Image src={ logo } alt="cinéfil logo" width={ 75 } height={ 75 }/>
+                </Link>
             </div>
             <div className="flex items-center gap-4">
-                <Search placeholder={ "Search movies..." }/>
-                <AccountDropdown/>
+                <Link href="/search" className="flex items-center gap-2">
+                    <SearchIcon/>
+                    Search
+                </Link>
+                <Suspense fallback={ null }>
+                    <AccountDropdown/>
+                </Suspense>
             </div>
         </div>
     )
