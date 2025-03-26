a8ac6936ec141fb1b21e5139618305ea71bc47b4
Matis Byar <matis.byar.06@icloud.com>
2025-03-26 22:40:32 +0100

    feat: integrate search inputs and results display into search page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,3 +1,6 @@
+import SearchInputs from "@/components/search/search-inputs";
+import SearchMoviesResult from "@/components/search/search-movies-result";
+
 interface SearchPageProps {
     searchParams: Promise<{
         query?: string;
@@ -8,10 +11,11 @@ export default async function SearchPage({searchParams}: SearchPageProps) {
     const {query} = await searchParams;
     
     return (
-        <main className="pt-24">
-            <h1>Search Page</h1>
+        <main className="flex flex-col w-full items-center flex-grow pt-24 h-[calc(100svh-7rem)] *:rounded-xl gap-6">
+            <h1 className="text-4xl font-bold text-center">Recherche</h1>
+            <SearchInputs/>
             
-            {query ?? "No query"}
+            <SearchMoviesResult query={ query }/>
         </main>
     )
 }
