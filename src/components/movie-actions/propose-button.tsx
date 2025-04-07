074acb8d5337be8a026a18b0685f4fac911b4a7f
Maxime Bodin <maxime.bodin@mailo.com>
2025-04-07 13:42:29 +0200

    Refactor propose-button.tsx.

--- src/components/movie-actions/propose-button.tsx
+++ src/components/movie-actions/propose-button.tsx
@@ -1,19 +1,22 @@
-import LoadingWheel from "@/components/ui/loading-wheel";
 import { Button } from "@/components/ui/button";
+import LoadingWheel from "@/components/ui/loading-wheel";
 
 interface ProposeButtonProps {
-    onClick: () => void,
-    loading: boolean
+  onClick: () => void;
+  loading: boolean;
 }
 
-export default function ProposeButton({onClick, loading}: ProposeButtonProps) {
-    return loading ? (
-            <Button className="w-full" disabled>
-                <LoadingWheel/>
-            </Button>)
-        : (
-            <Button className="w-full" onClick={ onClick }>
-                Proposer le film
-            </Button>
-        );
+export default function ProposeButton({
+  onClick,
+  loading,
+}: ProposeButtonProps) {
+  return (
+    <Button
+      onClick={onClick}
+      disabled={loading}
+      className="w-full py-2 text-base font-medium transition-all hover:opacity-90"
+    >
+      {loading ? <LoadingWheel /> : "🎬 Proposer le film"}
+    </Button>
+  );
 }
--- src/components/movies/movie-slide.tsx
+++ src/components/movies/movie-slide.tsx
@@ -1,12 +1,11 @@
-// MovieSlide.tsx
 "use client";
 
 import React from "react";
 import Link from "next/link";
 import { type EnhancedMovie } from "@/server/services/movie";
-import ProposeMovieManager from "@/components/propositions/proposeMovieManager";
-import VoteButton from "@/components/customButtons/vote-button";
 import { getHumanReadableDate, getYearOnly } from "@/utils/date";
+import ProposeMovieManager from "@/components/propositions/propose-movie-manager";
+import VoteButton from "@/components/movie-actions/vote-button";
 
 interface MovieSlideProps {
   movie: EnhancedMovie;
@@ -17,43 +16,45 @@ interface MovieSlideProps {
 }
 
 export default function MovieSlide({
-                                       movie,
-                                       filmCanBeProposed,
-                                       userHasVotedFor,
-                                       shownAt,
-                                       user,
-                                   }: MovieSlideProps) {
-    return (
-        <div className="embla__slide w-64 flex-shrink-0 px-2">
-            <Link href={`/movie/${movie.id}`}>
-                <div className="group cursor-pointer relative">
-                    <img
-                        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                        alt={movie.title}
-                        className={`h-96 w-full rounded-md object-cover ${
-                            shownAt ? "opacity-50 grayscale" : ""
-                        }`}
-                    />
-                    <div className="absolute bottom-0 left-0 right-0 flex h-1/2 flex-col justify-end rounded-b-md bg-gradient-to-t from-black via-transparent to-transparent p-4 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
-                        <h3 className="text-lg font-bold text-white">{movie.title}</h3>
-                        <p className="text-sm text-gray-500">{getYearOnly(movie.release_date)}</p>
-                        {shownAt && (
-                            <p className="pt-8 text-sm font-bold text-red-500">
-                                Diffusé {getHumanReadableDate(shownAt)}
-                            </p>
-                        )}
-                    </div>
-                </div>
-            </Link>
-            {!shownAt && user && (
-                <div className="mt-2">
-                    {filmCanBeProposed ? (
-                        <ProposeMovieManager movie={movie} />
-                    ) : (
-                        <VoteButton movieId={movie.id} initial={userHasVotedFor} />
-                    )}
-                </div>
+  movie,
+  filmCanBeProposed,
+  userHasVotedFor,
+  shownAt,
+  user,
+}: MovieSlideProps) {
+  return (
+    <div className="embla__slide w-64 flex-shrink-0 px-2">
+      <Link href={`/movie/${movie.id}`}>
+        <div className="group relative cursor-pointer">
+          <img
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            alt={movie.title}
+            className={`h-96 w-full rounded-md object-cover ${
+              shownAt ? "opacity-50 grayscale" : ""
+            }`}
+          />
+          <div className="absolute bottom-0 left-0 right-0 flex h-1/2 flex-col justify-end rounded-b-md bg-gradient-to-t from-black via-transparent to-transparent p-4 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+            <h3 className="text-lg font-bold text-white">{movie.title}</h3>
+            <p className="text-sm text-gray-500">
+              {getYearOnly(movie.release_date)}
+            </p>
+            {shownAt && (
+              <p className="pt-8 text-sm font-bold text-red-500">
+                Diffusé {getHumanReadableDate(shownAt)}
+              </p>
             )}
+          </div>
         </div>
-    );
+      </Link>
+      {!shownAt && user && (
+        <div className="mt-2">
+          {filmCanBeProposed ? (
+            <ProposeMovieManager movie={movie} />
+          ) : (
+            <VoteButton movieId={movie.id} initial={userHasVotedFor} />
+          )}
+        </div>
+      )}
+    </div>
+  );
 }
