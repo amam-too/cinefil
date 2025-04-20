cd9fcfe5d101408ec0f2f5fdafea26fd7942df79
Maxime Bodin <maxime.bodin@mailo.com>
2025-04-20 14:39:46 +0200

    Updating skeleton for movies actions.

--- src/components/movies/movies-skeleton.tsx
+++ src/components/movies/movies-skeleton.tsx
@@ -1,10 +1,11 @@
 import { Skeleton } from "@/components/ui/skeleton";
 
 export default function MoviesCarrouselSkeleton() {
-    return (
-        <div className="flex flex-col gap-4">
-            <Skeleton className="h-10 w-64" />
-            <Skeleton className="w-full h-64 rounded-lg mb-2"/>
-        </div>
-    )
+  return (
+    <div className="flex flex-col gap-4">
+      <Skeleton className="h-10 w-64" />
+      <Skeleton className="mb-2 h-96 w-full rounded-lg" />
+      <Skeleton className="h-10 w-full" />
+    </div>
+  );
 }
